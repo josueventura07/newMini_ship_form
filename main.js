c85d7effb0d60a6153ac7f0ea700aa6c1b631093
josueventura07 <josueandresventura@gmail.com>
2023-03-13 09:14:51 -0400

    feat: link send

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+const btnSend = document.querySelector('#btn_send')
 
+console.log(btnSend)
 
+btnSend.addEventListener('click', (e) => {
+    if(e.target) {
+        console.log('https://beamish-sundae-9d77fe.netlify.app/')
+    }
+})
