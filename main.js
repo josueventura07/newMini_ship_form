1a7b6f2d8736533a35e658c11decf4647eb1692b
josueventura07 <josueandresventura@gmail.com>
2023-03-15 15:13:31 -0400

    feat

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ function printItems() {
             html += `
                     <tr>
                         <th class="row">${item.id}</th>
-                        <td><input type="text" name="${item.type}" value="${item.type}"></td>
-                        <td><input type="text" name="${item.description}" value="${item.description}"></td>
-                        <td><input type="text" name="${item.um}" value="${item.um}"></td>
-                        <td><input type="number" name="${item.quantity}" value="${item.quantity}"></td>
+                        <td><input type="text" name="tipo" value="${item.type}"></td>
+                        <td><input type="text" name="descripcion" value="${item.description}"></td>
+                        <td><input type="text" name="unidad de medida" value="${item.um}"></td>
+                        <td><input type="number" name="cantidad" value="${item.quantity}"></td>
                         <td id="${item.id}">
                             <button class="btn_delete">Borrar</button>
                         </td>
